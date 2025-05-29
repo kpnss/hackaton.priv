94cece6466ae2213b0e755e13fb22c30aeb0727e
kpnss <155567420+kpnss@users.noreply.github.com>
2025-05-30 01:48:28 +0200

    Update loadData.py

--- src/loadData.py
+++ src/loadData.py
@@ -62,10 +62,9 @@ class GraphDataset(Dataset):
         return dictToGraphObject(self.graphs_dicts[idx])
 
     def _count_graphs(self):
-        with zipfile.ZipFile(self.raw, 'r') as zip_ref:
-            with zip_ref.open('graphs.json') as f:  # Sostituisci con il nome reale del file nel .zip
-                graphs_dicts = json.load(f)
-                return len(graphs_dicts), graphs_dicts
+        with open(self.raw, "r", encoding="utf-8") as f:
+            graphs_dicts = json.load(f)
+            return len(graphs_dicts), graphs_dicts
 
 def dictToGraphObject(graph_dict):
     edge_index = torch.tensor(graph_dict["edge_index"], dtype=torch.long)
